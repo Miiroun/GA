d6fe7683e5328bc27790380f2445872239f03ed6
Miiroun <106140031+Miiroun@users.noreply.github.com>
2023-05-12 23:23:10 +0200

    newFiles

---
+++ dataManipulate.c
@@ -0,0 +1,4 @@
+
+int readFile() {
+    return 1;
+}
---
+++ encrypt.c
@@ -0,0 +1,17 @@
+
+/*
+0 = rsa
+1 = 
+2 = 
+*/
+int cryptionType = 0;
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    
+    readFile();
+
+    return 0;
+}
