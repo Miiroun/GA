04981614a0ce3ab305cebc9cfe96123a86fcaa75
Miiroun <106140031+Miiroun@users.noreply.github.com>
2023-05-12 23:53:44 +0200

    test

--- dataManipulate.c
+++ dataManipulate.c
@@ -2,3 +2,13 @@
 int readFile() {
     return 1;
 }
+
+writeFile(char[] message) {
+    FILE *fptr;
+    
+    fptr = fopen("data/dataOut.txt", "w");
+
+    fprintf(fptr, message);
+    
+    fclose(fptr);
+}
