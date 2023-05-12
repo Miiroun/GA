a4d76f64ee7806c1b9f238d375dd6bc3ca02c044
Miiroun <106140031+Miiroun@users.noreply.github.com>
2023-05-13 00:35:46 +0200

    V4

--- encrypt.c
+++ encrypt.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 
 /*
-0 = rsa
-1 = 
-2 = 
+0 = des
+1 = aes
+2 = Diffie-Helman key exchange
 */
 int cryptionType = 0;
+int dataSize = 100;
+
+char desEncrypt(char dataIn[dataSize]) {
+  char encData[dataSize] = dataIn;
+  return encData;
+}
 
 
 int main()
 {
-    /* code */
-    //printf("Hello World!");
-    int x = dataManipulation.readFile();
+  FILE *inFile;
+  inFile = fopen("data/dataIn.txt", "r");
+  char dataIn[dataSize];
+
+  fgets(dataIn, dataSize, inFile);
+  printf("%s", dataIn);
+  fclose(inFile);
+
+  char encData[dataSize];
+  if (cryptionType == 0)
+  {
+    encData = desEncrypt(dataIn);
+  }
+
+  FILE *encFile;
+  encFile = fdopen("data/encryptedData.txt", "w");
+
+  fprintf(encFile, encData);
+
+  fclose(encFile);
 
   return 0;
 }
