0e5b2bfe974953820277e552bef8b3e9c383f25e
Miiroun <106140031+Miiroun@users.noreply.github.com>
2023-05-31 08:29:00 +0200

    working now

--- encrypt.c
+++ encrypt.c
@@ -11,34 +11,34 @@ aes
 Diffie-Helman key exchange
 */
 
-//int dataSize = 100;
+int dataSize = 256;
 
-char desEncrypt(char dataIn) 
+char * desEncrypt(char dataIn) 
 {
-  //char encData[100]; encData = dataIn;
-  return "hi"//encData;
+  char encData[256] = "Hi";
+  return &encData[0];
 }
 
 
 int main()
 {
-  enum EncryptionStandard cryptionType = DES;
+  enum EncryptionStandard cryptionType;
+  cryptionType = DES;
 
 
 
   FILE *inFile;
   inFile = fopen("data/dataIn.txt", "r");
-  char dataIn[100];
+  char dataIn[256] = "Hello";
 
   fgets(dataIn, dataSize, inFile);
   printf("%s", dataIn);
   fclose(inFile);
 
-  char encData[100];
-  encData[0] = "a";
+  char encData[256] = "a";
   if (cryptionType == DES)
   {
-    char tempData[100] = desEncrypt(dataIn);
+    char tempData[256] = "Hi";//desEncrypt(dataIn);
     for (size_t i = 0; i < dataSize; i++)
     {
       encData[i] = tempData[i];
