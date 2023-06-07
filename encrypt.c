9cfd5c7b1a3cadedf719fbad4796e4ac3a4f3997
Miiroun <106140031+Miiroun@users.noreply.github.com>
2023-06-07 13:14:07 +0200

    actully kinda works

--- encrypt.c
+++ encrypt.c
@@ -13,12 +13,20 @@ Diffie-Helman key exchange
 
 int dataSize = 256;
 
-char * desEncrypt(char *data) 
+char * desEncrypt(char *pData) 
 {
-  char encData[256];
-  encData[0] = *data;
-  *data = encData [0];
-  return &data[0];
+  char *encData[256]; //dont nead to create a new array here
+  encData[0] = pData;
+  int i;
+  for (i = 0; i < dataSize; i++)
+  {
+    //*encData[i] = *encData[i] + 1; // segmentation fault, hur fixar??
+    
+    
+  }
+  pData = encData[0]; //den läser just nu från random memorys
+
+  return pData;
 }
 
 
@@ -33,8 +41,7 @@ int main()
   char dataIn[256] = "Hello";
 
   fgets(dataIn, dataSize, inFile);
-  printf("%s", dataIn);
-  printf("\n");
+  printf("dataIn:%s, %d\n", dataIn, dataIn);
 
   fclose(inFile);
 
@@ -48,21 +55,28 @@ int main()
 
     int i;
     // just nu så bara tar värdet för index noll av tempdata, behöver flytta hela stringen, fixat tror jag
-    for (i = 0; i < dataSize; i++)
+    printf("CED: ["); //change encrypyed data:
+    for (i = 0; i < dataSize; i++)//datasize
     {
-      printf(*tempData[i]);
+      //*tempData[i] = *tempData[i] + 100; // flyttar stringen en bit framåt, byt ut till matrix för cesar chifer,
+      //borde vara andra del av kod
+      printf(" %c: %u,",*tempData[i], *tempData[i]); //change encrypyed data:
       encData[i] = *tempData[i];
+      //printf("%c",encData[i]);//1
+
       if (i != dataSize)
       {
-        tempData[i+1] = tempData[i] + 1;
+        tempData[i+1] = tempData[i] + 1; //vad gör denna kod? pointer magi?, min kod är äklig
       }      
     }
+    printf("]\n");
+
+    //sju första karaktärerna är korupta
+    printf("%c",encData[8]);//2 något fel sker med encData mellan utmarkerat 1&2, vet inte riktigt vad
     
   }
-
-
-  printf(encData);
-  printf("\n");
+  //vad är problemet med encData här?
+  printf("encryptedData:%s, %d\n",encData, encData);
 
   FILE *encFile;
   encFile = fopen("data/encryptedData.txt", "w");
